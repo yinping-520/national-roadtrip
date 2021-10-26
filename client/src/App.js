00652941b5b2fd0464341ddfb1d65cdec4a5434b
lweine01 <laurarweiner@gmail.com>
2021-10-26 14:58:51 -0400

    updated mutuations, and resolvers, debugged the token issues

--- client/src/App.js
+++ client/src/App.js
@@ -40,6 +40,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
+  console.log(token);
   return {
     headers: {
       ...headers,
