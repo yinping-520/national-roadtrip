5768f6d072719b319ff5b56e2efb56d1f813ecd8
thurston cox <thurstoncox3@gmail.com>
2021-10-27 10:23:40 -0400

    updated add/remove buttons to trips

--- client/src/components/Trips/index.js
+++ client/src/components/Trips/index.js
@@ -1,5 +1,7 @@
+import { parseAndCheckHttpResponse } from '@apollo/client';
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { useQueries } from '../../utils/queries';
 
 function Trips() {
 
@@ -17,7 +19,66 @@ function Trips() {
             </Marker>
         </MapContainer>
         </div>
-    );
+    )
+    
+    // const { parks, addPark, removePark } = useQueries();
+    // const [newTrip, setNewTrip] = useState('');
+
+    // return (
+    //     <div>
+    //         {parks ? (
+    //             <>
+    //                 <section className='park-list'>
+    //                     <table>
+    //                         <thead>
+    //                             <tr>
+    //                                 <th>ID</th>
+    //                                 <th>Park Name</th>
+    //                                 <th>State</th>
+    //                             </tr>
+    //                         </thead>
+
+    //                         <tbody>
+    //                             {parks.map((parks) => (
+    //                                 <tr key={park.id}>
+    //                                     <td>{park.id}</td>
+    //                                     <td>{park.name}</td>
+    //                                     <td>{park.state}</td>
+    //                                     <td>
+    //                                         <button
+    //                                             type='button'
+    //                                             onClick={() => removePark(park.id)}
+    //                                         >
+    //                                             <span role='img'
+    //                                                 aria-label='close'>
+    //                                                 X
+    //                                             </span>
+    //                                         </button>
+    //                                     </td>
+    //                                 </tr>
+    //                             ))}
+    //                         </tbody>
+    //                     </table>
+    //                     <div className='add-park'>
+    //                         <input
+    //                             onChang={() => setNewTrip(e.target.value)} placeholder='Add Next Trip!' type='text' value={newTrip}
+    //                         />
+    //                         <button
+    //                             type='button' onClick={() => {
+    //                                 addTrip({ name: newTrip });
+    //                                 setNewTrip('');
+    //                             }}
+    //                         >
+    //                             Add Trip
+    //                         </button>
+    //                     </div>
+    //                 </section>
+    //             </>
+    //         ) : (
+    //             <span>MMMmmm...stayin' home?</span>
+    //         )}
+    //     </div>
+    // )
 };
 
 export default Trips;
