590b8a3046795f635750503164768b48e1979325
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-10-26 21:02:13 -0400

    the marker is working on the map!

--- client/src/components/Trips/index.js
+++ client/src/components/Trips/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import {Icon} from 'leaflet'
+
 
 
 function Trips() {
@@ -15,13 +18,12 @@ function Trips() {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             />
-            <Marker position={position}>
+            <Marker position={position} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} >
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
             </Marker>
         </MapContainer>
-
     );
 };
 
