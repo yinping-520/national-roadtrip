d5ce93ee864bb94258cf2799029c9564729c3a27
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-10-27 17:01:36 -0400

    moved map to the parks js and removed map from the trips

--- client/src/pages/Park/index.js
+++ client/src/pages/Park/index.js
@@ -8,7 +8,11 @@ import { useParams } from 'react-router-dom'
 import up from "../../components/assets/up-arrow.jpg";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 import bikingArches from "../../components/assets/biking-arches.jpg";
-import Navbar from "../../components/Navbar/index";
+import 'leaflet/dist/leaflet.css';
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 
 function Park() {
   const { parkId } = useParams();
@@ -22,9 +26,14 @@ function Park() {
   const activities1 = park.activities1 || [];
   const activities2 = park.activities2 || [];
   const image = park.images || [];
+  const lat = park.lat || 38.733082;
+  const lng = park.long || -109.592514;
+  const [addItinerary] = useMutation(ADD_ITINERARY);
+  const position = [lat, lng];
 
-  console.log(activities1);
-  console.log(activities2);
+  console.log(data);
+
+
 
   const [current, setCurrent] = useState(0); // for next & prev slides
   const length = image.length;
@@ -47,94 +56,110 @@ function Park() {
   // const {loading, data} = useQuery()
 
   // const { addTrip } = Navbar();
-  const [addItinerary] = useMutation(ADD_ITINERARY);
 
   return (
     <div>
-      <div id="center-all">
-        <h1 className="park-name">{park.name}</h1>
-        <h2 className="city">{park.address}</h2>
-        <div className="slider">
-          <FaArrowAltCircleLeft
-            className="slider-icon left-arrow"
-            onClick={prevSlide}
-          />
-          <FaArrowAltCircleRight
-            className="slider-icon right-arrow"
-            onClick={nextSlide}
-          />
-          {image.map((slide, index) => {
-            return (
-              <div className="current-and-index">
-                <div
-                  className={index === current ? "slide active" : "slide"}
-                  key={index}
-                >
-                  {index === current && (
-                    <img src={slide} alt="images" className="image" />
-                  )}
-                </div>
-                {/* <div className="slider-total">
+      <div>
+        <div id="center-all">
+          <h1 className="park-name">{park.name}</h1>
+          <h2 className="city">{park.address}</h2>
+          <div className="slider">
+            <FaArrowAltCircleLeft
+              className="slider-icon left-arrow"
+              onClick={prevSlide}
+            />
+            <FaArrowAltCircleRight
+              className="slider-icon right-arrow"
+              onClick={nextSlide}
+            />
+            {image.map((slide, index) => {
+              return (
+                <div className="current-and-index">
+                  <div
+                    className={index === current ? "slide active" : "slide"}
+                    key={index}
+                  >
+                    {index === current && (
+                      <img src={slide} alt="images" className="image" />
+                    )}
+                  </div>
+                  {/* <div className="slider-total">
                   <img src={slide} alt="images" className="image-small" />
                 </div> */}
-              </div>
-            );
-          })}
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <a href="#top">
-        <img id="up-arrow" src={up} />
-      </a>
-      <div className="activities">
-        <h3 className="activity-header">Activities</h3>
-        <img id="bike" src={bikingArches} />
-        <div className="split-act">
-          <ul className="activity-list left">
-            {activities1.map((activities1, index) => (
+        <a href="#top">
+          <img id="up-arrow" src={up} />
+        </a>
+        <div className="activities">
+          <h3 className="activity-header">Activities</h3>
+          <img id="bike" src={bikingArches} />
+          <div className="split-act">
+            <ul className="activity-list left">
+              {activities1.map((activities1, index) => (
                 <li key={index}>
                   <i class="fas fa-campground"></i>
                   {activities1}
                 </li>
-            ))}
+              ))}
             </ul>
-          <ul className="activity-list right">
-            {activities2.map((activities2, index) => (
+            <ul className="activity-list right">
+              {activities2.map((activities2, index) => (
                 <li key={index}>
                   <i class="fas fa-campground"></i>
                   {activities2}
                 </li>
-            ))}
-          </ul>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className='description'>
+
+        </div>
+        <div className="weather-section">
+          <h4 className="weather-headline">Weather</h4>
+          <div className="weather-block">
+            <iframe src={park.weatherInfo} width='1000px' height='475px'></iframe>
+          </div>
+        </div>
+        <>
+          <button
+            type='button'
+            onClick={async () => {
+              const { data } = await addItinerary({
+                variables: {
+                  id: park._id,
+                }
+              });
+              console.log('add itinerary response', data)
+            }}
+          >
+            Add to Trip
+          </button>
+        </>
+        <div className="official-link">
+          <a href={park.website} target="_">
+            Visit the Official Nationals Park Site
+          </a>
         </div>
       </div>
-      <div className='description'>
-        
-      </div>
-      <div className="weather-section">
-        <h4 className="weather-headline">Weather</h4>
-        <div className="weather-block">
-          <iframe src={park.weatherInfo} width='1000px' height='475px'></iframe>
-        </div>
-      </div>
-      <>
-        <button
-          type='button'
-          onClick={async () => {
-            const { data } = await addItinerary({
-              variables: {
-                id: park._id,
-              }
-            });
-            console.log('add itinerary response', data)
-          }}
-        >
-          Add to Trip
-        </button>
-      </>
-      <div className="official-link">
-        <a href={park.website} target="_">
-          Visit the Official Nationals Park Site
-        </a>
+      <div>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{
+          height: "400px", width: "50%", backgroundColor: "red", marginTop: "80px", marginBottom: '90px'
+        }} >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={position} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })} >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   );
--- client/src/pages/Trips/index.js
+++ client/src/pages/Trips/index.js
@@ -1,36 +1,10 @@
 import { parseAndCheckHttpResponse } from '@apollo/client';
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 
 // import { useQueries } from '../../utils/queries';
-import 'leaflet/dist/leaflet.css';
-import markerIconPng from "leaflet/dist/images/marker-icon.png";
-import {Icon} from 'leaflet'
 
 
-
-function Trips() {
-
-    const position = [40.343182, -105.688103];
-
-    return (
-        <div>
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{
-            height: "400px", width: "50%", backgroundColor: "red", marginTop: "80px", marginBottom: '90px'
-        }} >
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            />
-            <Marker position={position} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} >
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
-        </div>
-    )
-    
     // const { parks, addPark, removePark } = useQueries();
     // const [newTrip, setNewTrip] = useState('');
 
