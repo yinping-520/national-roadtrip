50fe262ae4955e19783871ba9fff63186bc2aa4c
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-10-27 21:14:53 -0400

    added query, typedef, resolve for savedpark

--- client/src/pages/Trips/index.js
+++ client/src/pages/Trips/index.js
@@ -1,69 +1,22 @@
-import { parseAndCheckHttpResponse } from '@apollo/client';
 import React from 'react';
+import { QUERY_USER, QUERY_PARK_BY_UUID } from '../../utils/queries';
+import { useQuery, useMutation } from "@apollo/client";
+import { DELETE_ITINERARY } from '../../utils/mutations';
 
 
-// import { useQueries } from '../../utils/queries';
+function Trips() {
+    const { loading, data } = useQuery(QUERY_USER);
+    const itineraries = data.user.itinerary;
+    itineraries.map((itinerary) => {
 
+        const { loading, data } = useQuery(QUERY_PARK_BY_UUID);
 
-    // const { parks, addPark, removePark } = useQueries();
-    // const [newTrip, setNewTrip] = useState('');
-
-    // return (
-    //     <div>
-    //         {parks ? (
-    //             <>
-    //                 <section className='park-list'>
-    //                     <table>
-    //                         <thead>
-    //                             <tr>
-    //                                 <th>ID</th>
-    //                                 <th>Park Name</th>
-    //                                 <th>State</th>
-    //                             </tr>
-    //                         </thead>
-
-    //                         <tbody>
-    //                             {parks.map((parks) => (
-    //                                 <tr key={park.id}>
-    //                                     <td>{park.id}</td>
-    //                                     <td>{park.name}</td>
-    //                                     <td>{park.state}</td>
-    //                                     <td>
-    //                                         <button
-    //                                             type='button'
-    //                                             onClick={() => removePark(park.id)}
-    //                                         >
-    //                                             <span role='img'
-    //                                                 aria-label='close'>
-    //                                                 X
-    //                                             </span>
-    //                                         </button>
-    //                                     </td>
-    //                                 </tr>
-    //                             ))}
-    //                         </tbody>
-    //                     </table>
-    //                     <div className='add-park'>
-    //                         <input
-    //                             onChang={() => setNewTrip(e.target.value)} placeholder='Add Next Trip!' type='text' value={newTrip}
-    //                         />
-    //                         <button
-    //                             type='button' onClick={() => {
-    //                                 addTrip({ name: newTrip });
-    //                                 setNewTrip('');
-    //                             }}
-    //                         >
-    //                             Add Trip
-    //                         </button>
-    //                     </div>
-    //                 </section>
-    //             </>
-    //         ) : (
-    //             <span>MMMmmm...stayin' home?</span>
-    //         )}
-    //     </div>
-    // )
-    // );
+        
+        
+        return (<div>Hello</div>
+  
+        )  
+})
 };
 
 export default Trips;
