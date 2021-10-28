81886e8654a98d1de10d89d003755649c7b5472d
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-28 12:37:14 -0400

    trips page done

--- client/src/components/Journey/index.js
+++ client/src/components/Journey/index.js
@@ -1,67 +1,66 @@
-import '../css/mainpage.css';
-import '../css/scroll.css';
-import React from 'react';
-import signTurn from '../assets/turn-sign.jpg';
-import something from '../assets/something.jpg';
-import up from '../assets/up-arrow.jpg';
-import container from '../assets/journey-bg.jpg';
+// import '../css/mainpage.css';
+// import '../css/scroll.css';
+// import React from 'react';
+// import signTurn from '../assets/turn-sign.jpg';
+// import something from '../assets/something.jpg';
+// import up from '../assets/up-arrow.jpg';
 
-function Journey() {
-    return (
-        <div>
-            <div id='container'>
-                <img src={container}/>
-            </div>
-            <a href='#top'><img id='up-arrow' src={up}/></a>
-            <div className='site-section'>
-                <div id='sites1'>
-                    <div id='rocky-mtn' className='card'>
-                        <p>Rocky Mountain</p>
-                    </div>
-                    <div id='glacier' className='card'>
-                        <p>Glacier Mountain</p>
-                    </div>
-                    <div id='teton' className='card'>
-                        <p>Teton</p>
-                    </div>
-                    <div id='yosemite' className='card'>
-                        <p>Yosemite</p>
-                    </div>
-                </div>
-                <div id='destinations'>
-                    <img src={signTurn} alt={signTurn}/>
-                </div>
-                <div className='site-section'>
-                    <div id='sites2'>
-                        <div id='arches' className='card'>
-                            <p>Arches</p>
-                        </div>
-                        <div id='bryce' className='card'>
-                            <p>Bryce</p>
-                        </div>
-                        <div id='zion' className='card'>
-                            <p>Zion</p>                       
-                        </div>
-                        <div id='grand-canyon' className='card'>
-                            <p>Grand Canyon</p>
-                        </div>
-                    </div>
-                </div>
-                <div id='something'>
-                    <img src={something} alt={something}/>
-                </div>
-                <div id='sites3'>
-                    <div id='yellowstone' className='card'>
-                        <p>Yellowstone</p>
-                    </div>
-                    <div id='sequoia' className='card'>
-                        <p>Sequoia</p>
-                    </div>
-                </div>
-            </div>
+// function Journey() {
+//     return (
+//         <div>
+//             <div id='container'>
+//                 <img src={container}/>
+//             </div>
+//             <a href='#top'><img id='up-arrow' src={up}/></a>
+//             <div className='site-section'>
+//                 <div id='sites1'>
+//                     <div id='rocky-mtn' className='card'>
+//                         <p>Rocky Mountain</p>
+//                     </div>
+//                     <div id='glacier' className='card'>
+//                         <p>Glacier Mountain</p>
+//                     </div>
+//                     <div id='teton' className='card'>
+//                         <p>Teton</p>
+//                     </div>
+//                     <div id='yosemite' className='card'>
+//                         <p>Yosemite</p>
+//                     </div>
+//                 </div>
+//                 <div id='destinations'>
+//                     <img src={signTurn} alt={signTurn}/>
+//                 </div>
+//                 <div className='site-section'>
+//                     <div id='sites2'>
+//                         <div id='arches' className='card'>
+//                             <p>Arches</p>
+//                         </div>
+//                         <div id='bryce' className='card'>
+//                             <p>Bryce</p>
+//                         </div>
+//                         <div id='zion' className='card'>
+//                             <p>Zion</p>                       
+//                         </div>
+//                         <div id='grand-canyon' className='card'>
+//                             <p>Grand Canyon</p>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div id='something'>
+//                     <img src={something} alt={something}/>
+//                 </div>
+//                 <div id='sites3'>
+//                     <div id='yellowstone' className='card'>
+//                         <p>Yellowstone</p>
+//                     </div>
+//                     <div id='sequoia' className='card'>
+//                         <p>Sequoia</p>
+//                     </div>
+//                 </div>
+//             </div>
 
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
-export default Journey;
+// export default Journey;
--- client/src/pages/Trips/index.js
+++ client/src/pages/Trips/index.js
@@ -3,7 +3,8 @@ import { QUERY_USER } from '../../utils/queries';
 import { useQuery, useMutation } from "@apollo/client";
 import { DELETE_ITINERARY } from '../../utils/mutations';
 import auth from '../../utils/auth';
-
+import Park from '../Park';
+import '../../components/css/trips.css';
 
 function Trips() {
     console.log(auth.getProfile());
@@ -18,15 +19,35 @@ function Trips() {
     console.log(data);
     const itineraries = data.user.itinerary;
     return (<div>{itineraries.map((itinerary) => {
-
-
-          
         return (
-
-        <div>{itinerary.name}</div>
-
-        )  
-})}</div>)
+            <div className='trips-page'>
+                <div className='trip-container'>
+                    <div className='trip-name'>{itinerary.name} National Park</div>
+                    <ul className='activity-list trip-activities'>
+                        {itinerary.activities1.map((activities1, index) => (
+                            <li key={index}>
+                                {activities1}
+                            </li>
+                        ))}
+                    </ul>
+                    <div id={`${itinerary.id}`} className='card-dest trip-pic' alt='pic of national park'>
+                        <p>{itinerary.name}</p>
+                    </div>
+                    <div className='trip-links'>
+                        <a href={itinerary.website} target='_'>
+                            Official Park Site
+                        </a>
+                        <a href={itinerary.website} target='_'>
+                            Map Your Trip
+                        </a>
+                        <a href={itinerary.weatherInfo} target='_'>
+                            Weather Info
+                        </a>
+                    </div>
+                </div>
+            </div>
+        )
+    })}</div>)
 };
 
 export default Trips;
